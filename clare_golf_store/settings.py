73bc22aec655c29c62c3d8c2b76be01291e79acb
Jack kirkby <jkirkby95@gmail.com>
2024-05-02 17:16:16 +0000

    added shopping bag urls and template

--- clare_golf_store/settings.py
+++ clare_golf_store/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-jkirkby95-claregolfstor-3ksxommqg60.ws-eu110.gitpod.io',
+    '8000-jkirkby95-claregolfstor-3ksxommqg60.ws-eu112.gitpod.io',
 ]
 
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
