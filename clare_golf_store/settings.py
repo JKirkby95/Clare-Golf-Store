0e9c6471634d015f97dde1d32c74a6e909c14d68
Jack kirkby <jkirkby95@gmail.com>
2024-05-09 19:06:41 +0000

    added order history and confirmation email

--- clare_golf_store/settings.py
+++ clare_golf_store/settings.py
@@ -186,3 +186,5 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FROM_EMAIL = 'claregolfstore@example.com'
