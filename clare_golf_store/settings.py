7a311354516340c6ef24fb5356e2465cdfdaf7ff
Jack kirkby <jkirkby95@gmail.com>
2024-05-10 19:32:28 +0000

    add cloudinary to settings.py

--- clare_golf_store/settings.py
+++ clare_golf_store/settings.py
@@ -207,3 +207,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FROM_EMAIL = 'claregolfstore@example.com'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
+    'API_KEY': os.getenv("CLOUDINARY_API_KEY"),
+    'API_SECRET': os.getenv("CLOUDINARY_API_SECRET"),
+}
