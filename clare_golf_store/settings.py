19a4cccd50c13b41c26e8961eaf7baec86c0a1b2
Jack kirkby <jkirkby95@gmail.com>
2024-05-11 08:47:35 +0000

    testing cloudinary

--- clare_golf_store/settings.py
+++ clare_golf_store/settings.py
@@ -208,8 +208,4 @@ DEFAULT_FROM_EMAIL = 'claregolfstore@example.com'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
-    'API_KEY': os.getenv("CLOUDINARY_API_KEY"),
-    'API_SECRET': os.getenv("CLOUDINARY_API_SECRET"),
-}
+
