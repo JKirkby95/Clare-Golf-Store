9aa8a053850619e5a56063609b1b08d49727f01a
Jack kirkby <jkirkby95@gmail.com>
2024-05-16 18:54:10 +0000

    start contact form

--- clare_golf_store/settings.py
+++ clare_golf_store/settings.py
@@ -205,7 +205,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'claregolfstore@example.com'
+    DEFAULT_FROM_EMAIL = 'jkirkby95@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
--- home/views.py
+++ home/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ContactForm
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            # Send an email (for example)
+            send_mail(
+                f'Message from {name}',
+                message,
+                email,
+                ['jkirkby95@gmail.com'],
+            )
+            return render(request, 'contact/success.html')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form})
