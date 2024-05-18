be66e3177c0258bf5cb04a88954c1452b3a98baa
Jack kirkby <jkirkby95@gmail.com>
2024-05-18 11:24:29 +0000

    style contact us form

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,27 @@ from .models import ContactMessage
 
 
 class ContactForm(forms.ModelForm):
+    HEARD_ABOUT_CHOICES = [
+        ('', 'Select an option'),
+        ('search_engine', 'Search Engine'),
+        ('social_media', 'Social Media'),
+        ('friend', 'Friend'),
+        ('advertisement', 'Advertisement'),
+        ('other', 'Other'),
+    ]
+
+    heard_about_us = forms.ChoiceField(
+        choices=HEARD_ABOUT_CHOICES,
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = ContactMessage
-        fields = ['name', 'email', 'message']
+        fields = ['name', 'email', 'message', 'heard_about_us']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 5}),
+        }
--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+    heard_about_us = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
